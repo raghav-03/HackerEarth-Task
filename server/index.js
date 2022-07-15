df227cd7905888d9b11afeffe7dd395b83984116
Raghav Agarwal <agarwalraghav892@gmail.com>
2022-07-15 19:11:46 +0530

    fixed some bugs

--- server/index.js
+++ server/index.js
@@ -5,24 +5,18 @@ const dotenv=require('dotenv')
 const cloudinary=require('cloudinary');
 const bodyparser=require('body-parser');
 const mongoose = require('mongoose');
-
-// const fileupload=require('express-fileupload')
 const path=require('path');
 dotenv.config({path:"config/config.env"})
 app.use(Express.json());
 app.use(bodyparser.urlencoded({extended:true}))
-// app.use(fileupload());
 app.use(cors())
 app.use(require('./router/index.js'));
 require('./db/config')
-// app.use(Express.static(path.join(__dirname,"../client/build")))
-// app.get("*",(req,res)=>{
-//     res.sendFile(path.resolve(__dirname,"../client/build/index.html"))
-// })
-app.listen(process.env.Port,(e)=>{
+
+app.listen(process.env.PORT|| 3601,(e)=>{
     if(e){
         console.log("Error");
         return;
     }
-    console.log(`Server Running fine on port ${process.env.Port}`);
+    console.log(`Server Running fine on port ${process.env.PORT || 3601}`);
 });
